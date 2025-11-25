f31f59aa9d61a9022a09d1f06f6a874a9a858c29
rrr-us <H098S02206@chunchon.gwe.hs.kr>
2025-11-25 09:11:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,6 +74,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
+    labels[0] : {"texts" : ["중국식 냉면은 맛있어"],  "images" :
 }
 
 # ======================
